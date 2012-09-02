610117af19198067cea1ce38d60137687e7ecbf3
Christopher Antila <christopher.antila@mail.mcgill.ca>
2012-09-01 23:21:22 -0400

    eric4 files

--- vis-test.py
+++ vis-test.py
@@ -37,14 +37,14 @@ if __name__ == '__main__':
    print( "###############################################################################" )
    print( "" )
    # Suite for the interface/background components
-   unittest.TextTestRunner( verbosity = 2 ).run( Test_Settings.suite ) # works
+   #unittest.TextTestRunner( verbosity = 2 ).run( Test_Settings.suite ) # works
    #unittest.TextTestRunner( verbosity = 2 ).run( Test_Sorting.suite ) # works
    #unittest.TextTestRunner( verbosity = 2 ).run( Test_NGram.suite ) # works
    #unittest.TextTestRunner( verbosity = 2 ).run( Test_Vertical_Interval_Statistics.suite ) # works
    #unittest.TextTestRunner( verbosity = 2 ).run( Test_Output_Formatting.suite ) # broken, pending Issue #42
-   
+
    # Suites for the Analysis Engine
-   #unittest.TextTestRunner( verbosity = 2 ).run( Test_Analysis_Engine_Unit.suite ) # works
-   #unittest.TextTestRunner( verbosity = 2 ).run( Test_Analysis_Engine_Integration_Short.suite ) # works
-   #unittest.TextTestRunner( verbosity = 2 ).run( Test_Analysis_Engine_Integration_Long.suite ) # works
+   unittest.TextTestRunner( verbosity = 2 ).run( Test_Analysis_Engine_Unit.suite ) # works
+   unittest.TextTestRunner( verbosity = 2 ).run( Test_Analysis_Engine_Integration_Short.suite ) # works
+   unittest.TextTestRunner( verbosity = 2 ).run( Test_Analysis_Engine_Integration_Long.suite ) # works
    #unittest.TextTestRunner( verbosity = 2 ).run( Test_Fill_Space_Between_Offsets.suite ) # works
