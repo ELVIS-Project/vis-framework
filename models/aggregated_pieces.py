38f747558c907bf919bae3f76caf2f406d54c7a8
Christopher Antila <christopher.antila@mail.mcgill.ca>
2013-08-23 18:49:41 -0400

    Modify AggregatedPieces Method Signatures
    Implementation forthcoming...

--- models/aggregated_pieces.py
+++ models/aggregated_pieces.py
@@ -1,6 +1,5 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 #--------------------------------------------------------------------------------------------------
 # Program Name:           vis
 # Program Description:    Helps analyze music with computers.
@@ -31,15 +30,38 @@ class AggregatedPieces(object):
     Holds data from multiple IndexedPieces.
     """
 
+    class Metadata(object):
+        """
+        Hold aggregated metadata about the IndexedPieces in an AggregatedPiece. Every list has no
+        duplicate entries.
+
+        - composers: list of all the composers in the IndexedPieces
+        - dates: list of all the dates in the IndexedPieces
+        - date_range: 2-tuple with the earliest and latest dates in the IndexedPieces
+        - titles: list of all the titles in the IndexedPieces
+        - locales: list of all the locales in the IndexedPieces
+        - pathnames: list of all the pathnames in the IndexedPieces
+        """
+        __slots__ = (u'composers', u'dates', u'date_range', u'titles', u'locales', u'pathnames')
+
     # About the Data Model (for self._data)
     # =====================================
     # - ?
 
     # TODO: how to know which pieces are stored here?
 
-    def __init__(self, pathname):
+    def __init__(self, pieces):
+        """
+        Instantiate an AggregatedPieces.
+
+        Parameters
+        ==========
+        :param pieces: The IndexedPieces to collect.
+        :type pieces: list of IndexedPiece
+        """
         super(AggregatedPieces, self).__init__()
-        self.experiments = []
+        self._pieces = pieces
+        self._metadata = AggregatedPieces.Metadata()
 
     def __repr__(self):
         pass
@@ -50,16 +72,71 @@ class AggregatedPieces(object):
     def __unicode__(self):
         pass
 
-    def add_experiment(self, which_experiment, which_settings=None):
+    def metadata(self, field):
         """
-        ?
-        """
-        if not which_settings:
-            which_settings = {}
-        pass
+        Get a metadata about the IndexedPieces stored in this AggregatedPieces.
 
-    def remove_experiment(self, **args):
+        Valid fields are:
+        - composers: list of all the composers in the IndexedPieces
+        - dates: list of all the dates in the IndexedPieces
+        - date_range: 2-tuple with the earliest and latest dates in the IndexedPieces
+        - titles: list of all the titles in the IndexedPieces
+        - locales: list of all the locales in the IndexedPieces
+        - pathnames: list of all the pathnames in the IndexedPieces
+
+        Parameters
+        ==========
+        :param field: The name of the field to be accessed or modified
+        :type field: str or unicode
+
+        Returns
+        =======
+        :returns: object -- the field accessed, or None -- if assigning a field or attempting to
+            access a field that does not exist.
+
+        Raises
+        ======
+        :raises: TypeError -- if ``field`` is not a basestring, AttributeError -- if attempting
+            to set a field which is not in the :py:class:`IndexedPiece.Metadata` prototype.
         """
-        ?
+        if not isinstance(field, basestring):
+            raise TypeError(u"parameter 'field' must be of type 'basestring'")
+        elif hasattr(self._metadata, field):
+            return getattr(self._metadata, field)
+        else:
+            return None
+
+    def get_data(self, aggregated_experiments, independent_analyzers, settings=None):
+        """
+        Get the results of an Experimenter run on all the IndexedPieces. You must specify all
+        indexers and experimenters to be run to get the results you want.
+
+        The same settings dict will be given to all experiments and indexers.
+
+        If you want the results from all IndexedPieces separately, provide an empty list as the
+        "aggregated_experiments" argument.
+
+        Parameters
+        ==========
+        :param aggregated_experiments: The Experimenters to run on aggregated data of all pieces,
+            in the order you want to run them.
+        :type aggregated_experiments: list of types
+
+        :param independent_analyzers: The analyzers to run on each piece before aggregation, in the
+            order you want to run them.
+        :type independent_analyzers: list of types
+
+        :param settings: the settings to be used with all analyzers
+        :type settings: dict
+
+        Returns
+        =======
+        :return: Either one DataFrame with all experimental results or a list of DataFrames, each
+            with the experimental results for one piece.
+        :rtype: pandas.DataFrame or list of pandas.DataFrame
+
+        Raises
+        ======
+        TypeError: If the "analyzer_cls" is invalid or cannot be found.
         """
         pass
