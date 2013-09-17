9937db9edf5a0c2d655ef699046df5e743ae3357
Christopher Antila <christopher.antila@mail.mcgill.ca>
2013-09-17 19:22:57 -0400

    Relicense to AGPLv3+
    Headers changed in all files. License to come...

--- __init__.py
+++ __init__.py
@@ -13,18 +13,18 @@
 # Copyright (C) 2012, 2013 Christopher Antila, Jamie Klassen
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#-------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 """
 Package docstring: This is the root for our Sphinx documentation. Let's say something
 worthwhile like 'vis is an awesome program for data analysis with music'!
