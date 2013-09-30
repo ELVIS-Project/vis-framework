11acab24d19a11dd8cd7e2550aafd83040632db8
Christopher Antila <christopher.antila@mail.mcgill.ca>
2013-09-30 17:30:13 -0400

    Add Saining-Approved Colour to the Bars of Our Bar Charts

--- R_script.r
+++ R_script.r
@@ -6,7 +6,10 @@ R.test.results <- read.dta(args[1])
 ggplot() +
 geom_bar(aes(x=reorder(index, freq, function(x) x * -1), y=freq),
     data=R.test.results,
-    stat="identity") +
+    stat="identity",
+    colour = '#53869b',
+    fill = '#53869b') +
 scale_x_discrete("Interval") +
 scale_y_continuous("Frequency") +
+labs(title = "Interval Frequency for _______ Piece(s)") +
 ggsave(args[2])
