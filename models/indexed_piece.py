4499d1d41839df5973361b702ec64a8195281107
Christopher Antila <christopher.antila@mail.mcgill.ca>
2013-11-06 16:18:27 -0500

    Revise IndexedPiece and TemplateIndexer to Allow Score as Input
    There will be some cases where we want an entire stream.Score, not just
    its Part objects.

--- models/indexed_piece.py
+++ models/indexed_piece.py
@@ -330,6 +330,8 @@ class IndexedPiece(object):
             if analyzer_cls[0].required_score_type == stream.Part:
                 data = self._import_score()
                 data = [x for x in data.parts]  # Indexers require a list of Parts
+            elif analyzer_cls[0].required_score_type == stream.Score:  # TODO: test this
+                data = [self._import_score()]  # TODO: test this
             else:
                 msg = u'{} is missing required data from another analyzer.'.format(analyzer_cls[0])
                 raise RuntimeError(msg)
