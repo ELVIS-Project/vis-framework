95a26a1d6f4011965e8caffbafe806f7c06c0662
Christopher Antila <christopher.antila@mail.mcgill.ca>
2014-02-06 14:06:09 -0500

    Add LilyPond Tests to run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -30,8 +30,10 @@ VERBOSITY = 1
 
 # Imports
 import unittest
+<<<<<<< HEAD
 from vis.tests import test_indexer, test_note_rest_indexer, test_ngram, test_repeat, \
-    test_aggregator, test_interval_indexer, test_frequency_experimenter, test_offset
+    test_aggregator, test_interval_indexer, test_frequency_experimenter, test_offset, \
+    test_lilypond
 from vis.tests import test_indexed_piece, test_aggregated_pieces
 from vis.tests import bwv2_integration_tests as bwv2
 from vis.tests import test_workflow, test_workflow_integration, test_workflow_experiments
@@ -51,6 +53,10 @@ unittest.TextTestRunner(verbosity=VERBOSITY).run(test_repeat.REPEAT_INDEXER_SUIT
 unittest.TextTestRunner(verbosity=VERBOSITY).run(test_ngram.NGRAM_INDEXER_SUITE)
 unittest.TextTestRunner(verbosity=VERBOSITY).run(test_offset.OFFSET_INDEXER_SINGLE_SUITE)
 unittest.TextTestRunner(verbosity=VERBOSITY).run(test_offset.OFFSET_INDEXER_MULTI_SUITE)
+unittest.TextTestRunner(verbosity=VERBOSITY).run(test_lilypond.ANNOTATION_SUITE)
+unittest.TextTestRunner(verbosity=VERBOSITY).run(test_lilypond.ANNOTATE_NOTE_SUITE)
+unittest.TextTestRunner(verbosity=VERBOSITY).run(test_lilypond.PART_NOTES_SUITE)
+unittest.TextTestRunner(verbosity=VERBOSITY).run(test_lilypond.LILYPOND_SUITE)
 # Experimenter and Subclasses
 unittest.TextTestRunner(verbosity=VERBOSITY).run(test_frequency_experimenter.FREQUENCY_FUNC_SUITE)
 unittest.TextTestRunner(verbosity=VERBOSITY).run(test_frequency_experimenter.FREQUENCY_RUN_SUITE)
