7d6950003bffa42c80b0cb262d25450efd2b2a03
Christopher Antila <christopher.antila@mail.mcgill.ca>
2014-02-06 14:22:34 -0500

    Add Tests for lilypond.AnnotationIndexer

--- analyzers_tests/test_lilypond.py
+++ analyzers_tests/test_lilypond.py
@@ -31,71 +31,35 @@
 
 import unittest
 import pandas
-from vis.analyzers.indexers import template
-
-
-class TestTemplateIndexer(unittest.TestCase):
-    # NOTE: every method that is a test must be named with the "test_" prefix
-    def test_template_0(self):
-        # Test the indexer function alone...
-        # - with as few parts as valid
-        # - with as many parts as valid (or many parts)
-        # - with some parts, each with barely valid values
-        # - with some parts, each with decent
-        pass
-
-    def test_template_1(self):
-        # Example of how to replace the indexer function with a MagicMock.
-        # NB: This may not make sense for every indexer; you may be better off mocking
-        #     _do_multiprocessing() and ensuring that method gets the right arguments... or try
-        #     mocking both! But not at the same time.
-        #
-        # 1.) Prepare settings.
-        setts = {u'fake_setting': u'fake value'}
-        # 2.) Prepare the input values.
-        in_val = [pandas.Series([1, 2, 3]), pandas.Series([4, 5, 6])]
-        # 3.) Make the test indexer instance.
-        test_ind = template.TemplateIndexer(in_val, setts)
-        # 4.) Setup the mock.
-        with mock.patch(u'vis.analyzers.indexers.template.indexer_func') as mock_indfunc:
-            # 5.) The indexer_func() will always return a zero-length unicode string.
-            mock_indfunc.return_value = u''
-            # 6.) Run the indexer.
-            test_ind.run()
-            # 7.) Test the indexer_func() was called the right number of times (probably 3 in this
-            #     case, if we assume the two Series from "in_val" are treated as simultaneous parts
-            #     of the same piece.
-            self.assertEqual(3, len(mock_indfunc.call_args_list))
-            # 8.) The simultaneities that should have been given to indexer_func().
-            expected_calls = [[1, 4], [2, 5], [3, 6]]
-            # 9.) Everything in the "call_args_list" is a iterable "call" object. What you really
-            #     want is the first thing in that call, which is what the indexer_func() received.
-            actual_calls = [each[0] for each in mock_indfunc.call_args_list]
-            # 10.) Compare expected and actual. (NB: assertSequenceEqual() was new in Python 2.7)
-            self.assertSequenceEqual(expected_calls, actual_calls)
-
-    def test_template_2(self):
-        # Example for using the whole indexer; let's pretend this one adds a ^ over characters that
-        # support it, and removes those that don't.
-        # 1.) Prepare the input values.
-        in_val = [pandas.Series(['a', 'b', 'c', 'd'], index=[0.0, 0.5, 1.0, 1.5])]
-        # 2.) Prepare our expected output.
-        expected = [pandas.Series(['â', 'ĉ'], index=[0.0, 1.0])]
-        # 3.) Make then run the test indexer.
-        test_ind = TemplateIndexer(in_val)
-        actual = test_ind.run()
-        # 4.) Ensure we received the expected number of parts.
-        self.assertEqual(len(expected), len(actual))
-        # 5.) Compare each part...
-        for i in xrange(len(expected)):
-            # ... to know the index has the expected values, ...
-            self.assertSequenceEqual(list(expected[i].index), list(actual[i].index))
-            # ... and that the values are the expected values.
-            self.assertSequenceEqual(list(expected[i]), list(actual[i]))
+from vis.analyzers.indexers import lilypond
 
 
 class TestAnnotationIndexer(unittest.TestCase):
-    pass
+    def test_ind_func_1(self):
+        in_val = pandas.Series(['my shirt'])
+        expected = u'_\markup{ "my shirt" }'
+        actual = lilypond.annotation_func(in_val)
+        self.assertEqual(expected, actual)
+
+    def test_ind_func_2(self):
+        in_val = pandas.Series([42])
+        expected = u'_\markup{ "42" }'
+        actual = lilypond.annotation_func(in_val)
+        self.assertEqual(expected, actual)
+
+    def test_class_1(self):
+        in_val = [pandas.Series(['this', 'is']),
+                  pandas.Series(['how', 'we']),
+                  pandas.Series(['do', 'it']),
+                  pandas.Series(['baby', 'yeah'])]
+        expected = [pandas.Series(['_\markup{ "this" }', '_\markup{ "is" }']),
+                    pandas.Series(['_\markup{ "how" }', '_\markup{ "we" }']),
+                    pandas.Series(['_\markup{ "do" }', '_\markup{ "it" }']),
+                    pandas.Series(['_\markup{ "baby" }', '_\markup{ "yeah" }'])]
+        actual = lilypond.AnnotationIndexer(in_val).run()
+        for i in xrange(len(expected)):
+            self.assertSequenceEqual(list(expected[i].index), list(actual[i].index))
+            self.assertSequenceEqual(list(expected[i]), list(actual[i]))
 
 
 class TestAnnotateTheNoteIndexer(unittest.TestCase):
