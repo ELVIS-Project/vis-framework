e14929a7aeab5203fd18857b99e88d77f8e52694
Christopher Antila <christopher.antila@mail.mcgill.ca>
2014-02-06 14:51:30 -0500

    Add Tests for AnnotateTheNoteIndexer

--- analyzers_tests/test_lilypond.py
+++ analyzers_tests/test_lilypond.py
@@ -31,19 +31,20 @@
 
 import unittest
 import pandas
+from music21 import note
 from vis.analyzers.indexers import lilypond
 
 
 class TestAnnotationIndexer(unittest.TestCase):
     def test_ind_func_1(self):
         in_val = pandas.Series(['my shirt'])
-        expected = u'_\markup{ "my shirt" }'
+        expected = u'_\\markup{ "my shirt" }'
         actual = lilypond.annotation_func(in_val)
         self.assertEqual(expected, actual)
 
     def test_ind_func_2(self):
         in_val = pandas.Series([42])
-        expected = u'_\markup{ "42" }'
+        expected = u'_\\markup{ "42" }'
         actual = lilypond.annotation_func(in_val)
         self.assertEqual(expected, actual)
 
@@ -52,10 +53,10 @@ class TestAnnotationIndexer(unittest.TestCase):
                   pandas.Series(['how', 'we']),
                   pandas.Series(['do', 'it']),
                   pandas.Series(['baby', 'yeah'])]
-        expected = [pandas.Series(['_\markup{ "this" }', '_\markup{ "is" }']),
-                    pandas.Series(['_\markup{ "how" }', '_\markup{ "we" }']),
-                    pandas.Series(['_\markup{ "do" }', '_\markup{ "it" }']),
-                    pandas.Series(['_\markup{ "baby" }', '_\markup{ "yeah" }'])]
+        expected = [pandas.Series(['_\\markup{ "this" }', '_\\markup{ "is" }']),
+                    pandas.Series(['_\\markup{ "how" }', '_\\markup{ "we" }']),
+                    pandas.Series(['_\\markup{ "do" }', '_\\markup{ "it" }']),
+                    pandas.Series(['_\\markup{ "baby" }', '_\\markup{ "yeah" }'])]
         actual = lilypond.AnnotationIndexer(in_val).run()
         for i in xrange(len(expected)):
             self.assertSequenceEqual(list(expected[i].index), list(actual[i].index))
@@ -63,7 +64,29 @@ class TestAnnotationIndexer(unittest.TestCase):
 
 
 class TestAnnotateTheNoteIndexer(unittest.TestCase):
-    pass
+    def test_ind_func_1(self):
+        in_val = pandas.Series([u'_\\markup{ "my shirt" }'])
+        actual = lilypond.annotate_the_note(in_val)
+        self.assertTrue(isinstance(actual, note.Note))
+        self.assertTrue(hasattr(actual, u'lily_invisible'))
+        self.assertTrue(hasattr(actual, u'lily_markup'))
+        self.assertEqual(True, actual.lily_invisible)
+        self.assertEqual(u'_\\markup{ "my shirt" }', actual.lily_markup)
+
+    def test_class_1(self):
+        annotes = [['_\\markup{ "this" }', '_\\markup{ "is" }'],
+                   ['_\\markup{ "how" }', '_\\markup{ "we" }'],
+                   ['_\\markup{ "do" }', '_\\markup{ "it" }'],
+                   ['_\\markup{ "baby" }', '_\\markup{ "yeah" }']]
+        in_val = [pandas.Series(l) for l in annotes]
+        actual = lilypond.AnnotateTheNoteIndexer(in_val).run()
+        for i, each_part in enumerate(actual):
+            for j, each_note in enumerate(each_part):
+                self.assertTrue(isinstance(each_note, note.Note))
+                self.assertTrue(hasattr(each_note, u'lily_invisible'))
+                self.assertTrue(hasattr(each_note, u'lily_markup'))
+                self.assertEqual(True, each_note.lily_invisible)
+                self.assertEqual(annotes[i][j], each_note.lily_markup)
 
 
 class TestPartNotesIndexer(unittest.TestCase):
