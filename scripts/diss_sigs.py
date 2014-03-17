c6e66e5da77907d5dce6c2c6facdecf31f56ad95
Christopher Antila <christopher.antila@mail.mcgill.ca>
2014-03-17 00:42:01 -0400

    Add a quasi-SuspensionIndexer to the diss_sigs.py Script
    - I'm experimenting with a different sort of idea for indexers
    - we've got it picking up suspensions and other stuff
    - still have to fix how the LilyPond thing doesn't account for a note
      that doesn't start at the beginning of the score

--- scripts/diss_sigs.py
+++ scripts/diss_sigs.py
@@ -41,18 +41,26 @@ except ImportError:
 from vis.analyzers.indexers import noterest, interval, dissonance, metre
 from vis.models.indexed_piece import IndexedPiece
 from vis.workflow import WorkflowManager
+from numpy import nan as NaN
+import pandas
 
 # the piece we'll analyze... currently Kyrie of Palestrina's Missa "Dies sanctificatus"
-piece_path = u'test_corpus/bwv2.xml'
+piece_path = u'test_corpus/bwv77.mxl'
 the_piece = IndexedPiece(piece_path)
 
 # don't touch this (yet); it's settings required by DissonanceIndexer
 setts = {u'quality': True, 'simple or compound': u'simple'}
 
 # find the intervals
+notes = the_piece.get_data([noterest.NoteRestIndexer])
 intervals = the_piece.get_data([noterest.NoteRestIndexer,
                                 interval.IntervalIndexer],
                                setts)
+horiz_intervals = the_piece.get_data([noterest.NoteRestIndexer,
+                                interval.HorizontalIntervalIndexer],
+                               setts)
+
+
 
 # find the dissonances
 print(u'\n\nRunning the DissonanceIndexer...\n')
@@ -61,32 +69,83 @@ interv_input = [intervals[combo] for combo in interv_combos]
 dissonances = the_piece.get_data([dissonance.DissonanceIndexer], None, interv_input)
 dissonances = {interv_combos[i]: dissonances[i] for i in xrange(len(interv_combos))}
 
-# as an example, we'll just use voice-pair [0, 1] (highest and second-highest)
-#print(u'Output from DissonanceIndexer (top two voices):\n')
-#for label in intervals[u'0,1'].index:
-    #if dissonances[u'0,1'].loc[label] is None:
-        #print(str(label) + '\t' + str(intervals[u'0,1'].loc[label]))
-    #else:
-        #print(str(label) + '\t' + str(intervals[u'0,1'].loc[label]) + ' --> ' + str(dissonances[u'0,1'].loc[label]))
-
-
 # get and display the output from the "beatStrength" indexer
 print(u'\n\nRunning the NoteBeatStrengthIndexer...\n')
 beat_strengths = the_piece.get_data([metre.NoteBeatStrengthIndexer])
-#print(u'Output from NoteBeatStrengthIndexer (top voice):\n')
-#print(str(beat_strengths[0]))
 
+# as an example, we'll just use voice-pair [0, 1] (highest and second-highest)
+#print(u'Output (top two voices):\n')
+the_columns = pandas.MultiIndex.from_tuples([('NoteRestIndexer', 0), ('NoteRestIndexer', 1),
+                                             ('NoteBeatStrengthIndexer', 0), ('NoteBeatStrengthIndexer', 1),
+                                             ('HorizontalIntervalIndexer', 0), ('HorizontalIntervalIndexer', 1),
+                                             ('IntervalIndexer', '0,1'), ('DissonanceIndexer', '0,1')])
+new_df = pandas.DataFrame({0: notes[0],
+                           1: notes[1],
+                           2: beat_strengths[0],
+                           3: beat_strengths[1],
+                           4: horiz_intervals[0],
+                           5: horiz_intervals[1],
+                           6: intervals['0,1'],
+                           7: dissonances['0,1']})
+new_df.columns = the_columns
+#print(new_df)
+#print('=============\noffset 25.00:\n=============')
+#print(new_df.loc[25.00])
+
+
+# indexer function for SuspendionIndexer
+# - x: melodic interval of lower part into suspension, not unison (upper part is unison)
+# - d: dissonant harmonic interval
+# - y: melodic interval of lower part out of suspension (upper part is -2)
+# - z: d-y if y >= 1 else d-y-2 (it's the resolution vert-int)
+def susp_detector(one_row, next_row, offset):
+    # is there a dissonance?
+    if one_row['DissonanceIndexer']['0,1'] is not NaN:
+        # check x (lower part of melodic into diss)
+        if one_row['HorizontalIntervalIndexer'][1] == 'P1':
+            return 'other diss'
+        # set d (diss vert int)
+        d = int(one_row['DissonanceIndexer']['0,1'][-1:])
+        # set y (lower part melodic out of diss)
+        y = 1 if next_row['HorizontalIntervalIndexer'][1] is NaN else int(next_row['HorizontalIntervalIndexer'][1][-1:])
+        # set z (vert int after diss)
+        z = int(next_row['IntervalIndexer']['0,1'][-1:])
+        # deal with z
+        if (y >= y and d - y == z) or (d - y - 2 == z):
+            return 'susp'
+        else:
+            return 'other diss'
+    else:
+        return NaN
+
+# run() for SuspensionIndexer
+results = []
+for i in xrange(len(new_df.index) - 1):
+    results.append(susp_detector(new_df.iloc[i], new_df.iloc[i + 1], i))
+results.append(NaN)  # because we did the "- 1" thing in the loop above
+
+# This is a bit of a hack to get the new results into the existing MultiIndex DataFrame...
+# I still need to figure out how to specify both levels, so we can specify voice pairs
+new_df.T.loc['SuspensionIndexer'] = None
+new_df['SuspensionIndexer'] = pandas.Series(results, index=new_df.index)
+
+# output the whole DataFrame to a CSV file, for easy viewing
+new_df.to_csv('test_output/nice_results.csv')
+
+## LilyPond Output! ##
 # break a WorkflowManager so we can get annotated score output
 print(u'\n\nPreparing and outputting the score, running LilyPond, etc.\n')
 # 1.) collect indicces for this part combo
-part_diss_orig = dissonances[u'0,1']
-beats_zero_orig = beat_strengths[0]
-beats_one_orig = beat_strengths[1]
+#part_diss_orig = dissonances[u'0,1']
+#beats_zero_orig = beat_strengths[0]
+#beats_one_orig = beat_strengths[1]
 # 2.) filter out where there isn't a dissonance
-part_diss = part_diss_orig[~part_diss_orig.isin([None])]
-beats_zero = beats_zero_orig[~part_diss_orig.isin([None])]
-beats_one = beats_one_orig[~part_diss_orig.isin([None])]
+#susp_index = new_df['SuspensionIndexer']
+#susp_index = susp_index[susp_index == 'susp']
+#part_diss = part_diss_orig[~part_diss_orig.isin([None])]
+#beats_zero = beats_zero_orig[~part_diss_orig.isin([None])]
+#beats_one = beats_one_orig[~part_diss_orig.isin([None])]
 # 3.) mangle the WorkflowManager
-workm = WorkflowManager([piece_path])
-workm._result = [[part_diss, beats_zero, beats_one]]
-workm.output('LilyPond', 'test_output/asdf_diss')
+#workm = WorkflowManager([piece_path])
+#workm._result = [[susp_index]]
+#workm.output('LilyPond', 'test_output/asdf_diss')
