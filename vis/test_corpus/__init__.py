b56ad98098f5fa2d2b98a277278199aa63f9c064
Christopher Antila <christopher.antila@mail.mcgill.ca>
2014-03-22 15:17:14 -0400

    ... how did the test_corpus get lost? ...

---
+++ vis/test_corpus/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#--------------------------------------------------------------------------------------------------
+# Program Name:              vis
+# Program Description:       Measures sequences of vertical intervals.
+#
+# Filename: test_corpus/__init__.py
+# Purpose: Import the native music21 tests.
+#
+# Copyright (C) 2012, 2013 Christopher Antila, Jamie Klassen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------
+"""
+Import the native music21 tests.
+"""
+
+__all__ = ['int_indexer_short']
+
+from vis.test_corpus import int_indexer_short
