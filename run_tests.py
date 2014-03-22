de4660591be284d88d2a9f84c6b4912dc8ab5b29
Christopher Antila <christopher.antila@mail.mcgill.ca>
2014-03-22 16:35:59 -0400

    remove silly import from run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -26,14 +26,6 @@
 Run the tests for the models
 """
 
-# Ensure we can import "vis"
-import imp
-try:
-    imp.find_module(u'vis')
-except ImportError:
-    import sys
-    sys.path.insert(0, u'..')
-
 VERBOSITY = 1
 
 # Imports
