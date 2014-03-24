a19e051d3c31711496784655004bdb4070825214
Christopher Antila <christopher.antila@mail.mcgill.ca>
2014-03-23 21:59:05 -0400

    Starting to Get Distutils Stuff...
    ... all that's left is the docs, I think.

--- setup.py
+++ setup.py
@@ -1,19 +1,70 @@
-# setup.py for the VIS Framework
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#--------------------------------------------------------------------------------------------------
+# Program Name:           vis
+# Program Description:    Helps analyze music with computers.
+#
+# Filename:               setup.py
+# Purpose:                Distutils Information for the VIS Framework
+#
+# Copyright (C) 2014 Christopher Antila
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------
+"""
+.. codeauthor:: Christopher Antila <christopher@antila.ca>
+
+Distutils information for the VIS Framework.
+"""
 
 from distutils.core import setup
 
 
+VERSION = '1.0.0'
+
 setup(
     name = "vis-framework",
-    packages = [""],
-    version = "1.0.0",
-    description = "",
-    author = "Christopher Antila",
+    version = VERSION,
+    description = "The VIS Framework for Music Analysis",
+    author = "Christopher Antila, Jamie Klassen",
     author_email = "christopher@antila.ca",
     license = "AGPLv3+",
-    url = "http://elvisproject.ca/api",
-    download_url = "http://elvisproject.ca/download/framework-latest.tar.xz",
+    url = "http://elvisproject.ca/api/",
+    download_url = "http://elvisproject.ca/download/framework-%s.tar.bz2" % VERSION,
     keywords = [],
+    provides = [
+        'vis',
+        # 'outputlilypond',
+        ],
+    requires = [
+        # NB: keep this in sync with vis/requirements.txt and vis/optional_requirements.txt
+        # NB2: I left out "tables" since afaik nobody uses our HDF5 output...
+        'music21 (>=1.7.1)',
+        'pandas (>=0.12.0, <0.14)',
+        'mock (>=1.0.1)',
+        'numexpr (>=2.2.2)',
+        'Bottleneck (>=0.7.0)',
+        'openpyxl (>=1.6.2)',
+        ],
+    packages = [
+        'vis',
+        'vis.models',
+        'vis.analyzers',
+        'vis.analyzers.indexers',
+        'vis.analyzers.experimenters',
+        ],
+    package_data = {'vis': ['scripts/*']},
     classifiers = [
         "Programming Language :: Python",
         "Natural Language :: English",
@@ -34,8 +85,8 @@ setup(
         "Topic :: Scientific/Engineering :: Information Analysis",
         ],
     long_description = """\
-The VIS Music Analysis Framework
---------------------------------
+The VIS Framework for Music Analysis
+------------------------------------
 
 VIS is a Python package that uses the music21 and pandas libraries to build a ridiculously flexible and preposterously easy system for writing computer music analysis programs.
 """
