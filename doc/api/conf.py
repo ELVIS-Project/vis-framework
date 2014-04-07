cdd9092fa455cfcde608a5ce681b3f2d035fe6e8
Christopher Antila <christopher@antila.ca>
2014-04-06 23:53:40 -0400

    (temporarily?) switch back to 'sphinxdoc' API theme
    The sphinx_rtd_theme theme has a few difficult-to-read things, and I
    haven't had time to work them out yet.

--- doc/api/conf.py
+++ doc/api/conf.py
@@ -12,7 +12,7 @@
 # serve to show the default.
 
 import sys, os
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 
 # Ensure we can import "vis"
 import imp
@@ -102,7 +102,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -110,7 +111,7 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
