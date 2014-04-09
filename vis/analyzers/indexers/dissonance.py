9be96f9ceec521c08d7d61220e500e850c829ee4
Christopher Antila <christopher@antila.ca>
2014-04-08 22:24:49 -0400

    DissonanceIndexer Returns a DataFrame (and Recognizes Consonant P4s)

--- vis/analyzers/indexers/dissonance.py
+++ vis/analyzers/indexers/dissonance.py
@@ -31,55 +31,144 @@ Indexers related to dissonance.
 # disable "string statement has no effect" warning---they do have an effect with Sphinx!
 # pylint: disable=W0105
 
+from numpy import nan
+import pandas
 from vis.analyzers import indexer
 
 
 class DissonanceIndexer(indexer.Indexer):
     """
-    Remove consonant intervals. All remaining intervals are a "dissonance."
-
-    The following intervals are curently considered "consonant": P1, m3, M3, P5, m6, M6, and P8.
+    Remove consonant intervals. All remaining intervals are a "dissonance." Refer to the
+    :const:`CONSONANCES` constant for a list of consonances. A perfect fourth is sometimes
+    consonant---refer to the ``'special_P4'`` setting for more information.
     """
 
-    required_score_type = 'pandas.Series'
-    """
-    Provide a list of :class:`pandas.Series`.
-    """
+    CONSONANCES = [u'P1', u'm3', u'M3', u'P5', u'm6', u'M6', u'P8']
+    _CONSONANCE_MAKERS = [u'm3', u'M3', u'm6', u'M6']
 
-    possible_settings = []
+    required_score_type = 'pandas.DataFrame'
+    default_settings = {u'special_P4': True}
+    possible_settings = [u'special_P4']
     """
-    There are no settings?
-    """
-
-    default_settings = {}
-    """
-    There are no settings?
+    :keyword bool 'special_P4': Whether to account for the Perfect Fourth's "special"
+        characteristic of being a dissonance only when no major or minor third or sixth appears
+        below it. If this is ``True``, an additional indexing process is run that removes all
+        fourths "under" which the following intervals appear: m3, M3, m6, M6.
     """
 
     def __init__(self, score, settings=None):
         """
         :param score: The output from :class:`~vis.analyzers.indexers.interval.IntervalIndexer`.
-            You must include interval quality and *not* use compound intervals.
-        :type score: `list`` of :class:`pandas.Series`
-
-        :param settings: There are no settings.
-        :type settings: ``None``
+            You *must* include interval quality and *must not* use compound intervals.
+        :type score: :class:`pandas.DataFrame`
+        :param NoneType settings: There are no settings.
 
         :raises: :exc:`RuntimeError` if ``score`` is the wrong type.
         :raises: :exc:`RuntimeError` if ``score`` is not a list of the same types.
         """
+        self._settings = {u'special_P4': DissonanceIndexer.default_settings[u'special_P4']}
+        if settings is not None and u'special_P4' in settings:
+            self._settings[u'special_P4'] = settings[u'special_P4']
         super(DissonanceIndexer, self).__init__(score, None)
+
+    @staticmethod
+    def nan_consonance(simul):
+        """
+        Used internally by the :class:`DissonanceIndexer`.
+
+        Convert all "consonant" values to nan. The "consonant" intervals are those contained in
+        :const:`DissonanceIndexer.CONSONANCES`.
+
+        :param simul: A simultaneity with arbitrary vertical intervals.
+        :type simul: :class:`pandas.Series`
+
+        :returns: A :class:`Series` of the same length, where all "consonances" have been replaced
+            with nan.
+        :rtype: :class:`pandas.Series`
+        """
+        return simul.map(lambda x: nan if x in DissonanceIndexer.CONSONANCES else x)
+
+    @staticmethod
+    def special_fourths(simul):
+        """
+        Used internally by the :class:`DissonanceIndexer`.
+
+        Replace all consonant fourths in a :class:`Series` with nan. The method checks each part
+        combination and, if it finds a ``'P4'``, checks all part combinations for a major or minor
+        third or sixth sounding below the lower note of the fourth.
+
+        For example, consider the following simultaneity:
+
+        +------------------+----------+
+        | Part Combination | Interval |
+        +==================+==========+
+        | 0,1              | M3       |
+        +------------------+----------+
+        | 0,2              | P4       |
+        +------------------+----------+
+        | 0,3              | M3       |
+        +------------------+----------+
+        | 1,2              | M3       |
+        +------------------+----------+
+        | 1,3              | M3       |
+        +------------------+----------+
+        | 2,3              | P8       |
+        +------------------+----------+
+
+        On encountering ``'P4'`` in the ``'0,2'`` part combination, :meth:`_special_fourths` only
+        looks at the ``'2,3'`` combination for a third or sixth. Finding an octave, this fourth
+        is considered "dissonant," and therefore retained.
+
+        For this reason, it's very important that the index has good part-combination labels that
+        follow the ``'int,int'`` format.
+        """
+        post = []
+        for combo in simul.index:
+            if u'P4' == simul.loc[combo]:
+                lower_voice = combo.split(u',')[1]
+                investigate_these = []
+                for possibility in simul.index:
+                    if possibility.split(u',')[0] == lower_voice:
+                        investigate_these.append(possibility)
+                found_one = False
+                for possibility in investigate_these:
+                    if simul.loc[possibility] in DissonanceIndexer._CONSONANCE_MAKERS:
+                        found_one = True
+                        break
+                if found_one:
+                    post.append(nan)
+                else:
+                    post.append(simul.loc[combo])
+            else:
+                post.append(simul.loc[combo])
+        return pandas.Series(post, index=simul.index)
 
     def run(self):
         """
         Make a new index of the piece.
 
-        Returns
-        =======
-        :returns: A list of the new indices. The index of each Series corresponds to the index of
-            the Part used to generate it, in the order specified to the constructor. Each element
-            in the Series is a basestring.
-        :rtype: ``list`` of :class:`pandas.Series`
+        :returns: A :class:`DataFrame` of the new indices. The columns have a :class:`MultiIndex`;
+            refer to the example below for more details.
+        :rtype: :class:`pandas.DataFrame`
+
+        **Example:**
+
+        >>> the_score = music21.converter.parse('sibelius_5-i.mei')
+        >>> the_score.parts[5]
+        (the first clarinet Part)
+        >>> the_notes = NoteRestIndexer(the_score).run()
+        >>> the_notes['noterest.NoteRestIndexer']['5']
+        (the first clarinet Series)
+        >>> the_intervals = IntervalIndexer(the_notes).run()
+        >>> the_intervals['interval.IntervalIndexer']['5,6']
+        (Series with vertical intervals between first and second clarinet)
+        >>> the_dissonances = DissonanceIndexer(the_notes).run()
+        >>> the_intervals['dissonance.DissonanceIndexer']['5,6']
+        (Series with only the dissonant intervals between first and second clarinet)
         """
-        results = [p.loc[~p.str.contains(r'P1|m3|M3|P5|m6|M6|P8')] for p in self._score]
-        return results
+        post = self._score['interval.IntervalIndexer']
+        if self._settings[u'special_P4'] is True:
+            post = post.apply(DissonanceIndexer.special_fourths, axis=1)
+        post = post.apply(DissonanceIndexer.nan_consonance, axis=1)
+        return self.make_return([x for x in post.columns],
+                                [post[x] for x in post.columns])
--- vis/tests/test_dissonance.py
+++ vis/tests/test_dissonance.py
@@ -31,61 +31,218 @@
 
 
 import unittest
+from numpy import nan, isnan
 import pandas
 from vis.analyzers.indexers import dissonance
 
 
 class TestDissonanceIndexer(unittest.TestCase):
-    def test_ind_1(self):
+    @staticmethod
+    def make_dataframe(name, labels, from_these):
+        """
+        Make a DataFrame in the style that comes from an Indexer.run()
+
+        :param basestring name: The name of the indexer you're pretending to be.
+        :param labels: The labels for each elements of ``from_these``.
+        :type labels: list of basestring
+        :param from_these: The Series to put as the new DataFrame's columns.
+        :type from_these: list of :class:`pandas.Series`
+
+        :returns: A DataFrame in the style of an Indexer.run()
+        :rtype: :class:`pandas.DataFrame`
+        """
+        tuples = [(name, labels[i]) for i in xrange(len(labels))]
+        multiindex = pandas.MultiIndex.from_tuples(tuples, names=['Indexer', 'Parts'])
+        return pandas.DataFrame(from_these, index=multiindex).T
+
+    def test_nancons_1(self):
         # that it picks up simple major/minor things as dissonant
         in_vals = [u'm2', u'M2', u'P4', u'm7', u'M7']
-        for val in in_vals:
-            expected = pandas.Series([val])
-            actual = dissonance.DissonanceIndexer([expected]).run()[0]
-            self.assertSequenceEqual(list(expected.index), list(actual.index))
-            self.assertSequenceEqual(list(expected.values), list(actual.values))
-
-    def test_ind_2(self):
-        # that it picks up diminished/augmented dissonances
-        in_vals = ['A4', 'd5', 'A1', 'd8']
-        for val in in_vals:
-            expected = pandas.Series([val])
-            actual = dissonance.DissonanceIndexer([expected]).run()[0]
-            self.assertSequenceEqual(list(expected.index), list(actual.index))
-            self.assertSequenceEqual(list(expected.values), list(actual.values))
-
-    def test_ind_3(self):
-        # that consonances are properly noted as consonant
-        in_vals = [u'm3', u'M3', u'P5', u'm6', u'M6']
-        for val in in_vals:
-            in_ser = pandas.Series([val])
-            act = dissonance.DissonanceIndexer([in_ser]).run()[0]
-            self.assertEqual(0, len(act))
-
-    def test_ind_4(self):
-        # a quasi-realistic, single-part test
-        in_ser = pandas.Series(['m3', 'M2', 'm2', 'P4', 'd5', 'P5', 'M6', 'm7'])
-        expected = pandas.Series(['M2', 'm2', 'P4', 'd5', 'm7'], index=[1, 2, 3, 4, 7])
-        actual = dissonance.DissonanceIndexer([in_ser]).run()[0]
+        expected = pandas.Series(in_vals)
+        actual = dissonance.DissonanceIndexer.nan_consonance(expected)
         self.assertSequenceEqual(list(expected.index), list(actual.index))
         self.assertSequenceEqual(list(expected.values), list(actual.values))
 
-    def test_ind_5(self):
-        # a quasi-realistic, multi-part test
-        in_sers = [pandas.Series(['m3', 'M2', 'm2', 'P4', 'd5', 'P5', 'M6', 'm7']),
-                   pandas.Series(['M2', 'm2', 'P4', 'd5', 'P5', 'M6', 'm7', 'm3']),
-                   pandas.Series(['m2', 'P4', 'd5', 'P5', 'M6', 'm7', 'm3', 'M2'])]
-        expected = [pandas.Series(['M2', 'm2', 'P4', 'd5', 'm7'], index=[1, 2, 3, 4, 7]),
-                    pandas.Series(['M2', 'm2', 'P4', 'd5', 'm7'], index=[0, 1, 2, 3, 6]),
-                    pandas.Series(['m2', 'P4', 'd5', 'm7', 'M2'], index=[0, 1, 2, 5, 7])]
-        actual = dissonance.DissonanceIndexer(in_sers).run()
-        self.assertEqual(len(expected), len(actual))
-        for i in xrange(len(expected)):
-            self.assertSequenceEqual(list(expected[i].index), list(actual[i].index))
-            self.assertSequenceEqual(list(expected[i].values), list(actual[i].values))
+    def test_nancons_2(self):
+        # that it picks up diminished/augmented dissonances
+        in_vals = ['A4', 'd5', 'A1', 'd8']
+        expected = pandas.Series(in_vals)
+        actual = dissonance.DissonanceIndexer.nan_consonance(expected)
+        self.assertSequenceEqual(list(expected.index), list(actual.index))
+        self.assertSequenceEqual(list(expected.values), list(actual.values))
 
-    # NOTE: I decided not to test the rest of this indexer because it uses a pattern very common
-    #       through the rest of the Framework.
+    def test_nancons_3(self):
+        # that consonances are properly noted as consonant
+        in_vals = [u'm3', u'M3', u'P5', u'm6', u'M6']
+        actual = dissonance.DissonanceIndexer.nan_consonance(pandas.Series(in_vals))
+        self.assertEqual(len(in_vals), len(actual))
+        self.assertTrue(all([isnan(x) for x in actual]))
+
+    def test_nancons_4(self):
+        # a quasi-realistic, single-part test
+        in_ser = pandas.Series(['m3', 'M2', 'm2', 'P4', 'd5', 'P5', 'M6', 'm7'])
+        expected = pandas.Series([nan, 'M2', 'm2', 'P4', 'd5', nan, nan, 'm7'])
+        actual = dissonance.DissonanceIndexer.nan_consonance(in_ser)
+        self.assertSequenceEqual(list(expected.index), list(actual.index))
+        self.assertSequenceEqual(list(expected.values), list(actual.values))
+
+    def test_special_fourths_1(self):
+        """
+        +------------------+----------+
+        | Part Combination | Interval |
+        +==================+==========+
+        | 0,1              | M3       |
+        +------------------+----------+
+        | 0,2              | P4       |  retained
+        +------------------+----------+
+        | 0,3              | M3       |
+        +------------------+----------+
+        | 1,2              | M3       |
+        +------------------+----------+
+        | 1,3              | M3       |
+        +------------------+----------+
+        | 2,3              | P8       |
+        +------------------+----------+
+        """
+        in_series = pandas.Series(['M3', 'P4', 'M3', 'M3', 'M3', 'P8'],
+                                  index=['0,1', '0,2', '0,3', '1,2', '1,3', '2,3'])
+        expected = pandas.Series(['M3', 'P4', 'M3', 'M3', 'M3', 'P8'],
+                                 index=['0,1', '0,2', '0,3', '1,2', '1,3', '2,3'])
+        actual = dissonance.DissonanceIndexer.special_fourths(in_series)
+        self.assertSequenceEqual(list(expected.index), list(actual.index))
+        self.assertSequenceEqual(list(expected.values), list(actual.values))
+
+    def test_special_fourths_2(self):
+        """
+        +------------------+----------+
+        | Part Combination | Interval |
+        +==================+==========+
+        | 0,1              | P4       |  removed
+        +------------------+----------+
+        | 0,2              | P4       |  retained
+        +------------------+----------+
+        | 0,3              | M3       |
+        +------------------+----------+
+        | 1,2              | P8       |
+        +------------------+----------+
+        | 1,3              | M3       |
+        +------------------+----------+
+        | 2,3              | P8       |
+        +------------------+----------+
+        """
+        in_series = pandas.Series(['P4', 'P4', 'M3', 'P8', 'M3', 'P8'],
+                                  index=['0,1', '0,2', '0,3', '1,2', '1,3', '2,3'])
+        expected = pandas.Series([nan, 'P4', 'M3', 'P8', 'M3', 'P8'],
+                                 index=['0,1', '0,2', '0,3', '1,2', '1,3', '2,3'])
+        actual = dissonance.DissonanceIndexer.special_fourths(in_series)
+        self.assertSequenceEqual(list(expected.index), list(actual.index))
+        for i in xrange(len(expected)):
+            # NB: if you get a TypeError here, that means failure
+            self.assertTrue((expected.iloc[i] == actual.iloc[i]) or
+                            (isnan(expected.iloc[i]) and isnan(actual.iloc[i])))
+
+    def test_special_fourths_3(self):
+        """
+        +------------------+----------+
+        | Part Combination | Interval |  all retained
+        +==================+==========+
+        | 0,1              | M3       |
+        +------------------+----------+
+        | 0,2              | P5       |
+        +------------------+----------+
+        | 0,3              | M3       |
+        +------------------+----------+
+        | 1,2              | M3       |
+        +------------------+----------+
+        | 1,3              | M3       |
+        +------------------+----------+
+        | 2,3              | P8       |
+        +------------------+----------+
+        """
+        in_series = pandas.Series(['M3', 'P5', 'M3', 'M3', 'M3', 'P8'],
+                                  index=['0,1', '0,2', '0,3', '1,2', '1,3', '2,3'])
+        expected = pandas.Series(['M3', 'P5', 'M3', 'M3', 'M3', 'P8'],
+                                 index=['0,1', '0,2', '0,3', '1,2', '1,3', '2,3'])
+        actual = dissonance.DissonanceIndexer.special_fourths(in_series)
+        self.assertSequenceEqual(list(expected.index), list(actual.index))
+        self.assertSequenceEqual(list(expected.values), list(actual.values))
+
+
+    def test_ind_1(self):
+        # A quasi-realistic, multi-part test---special_P4 is False
+        in_val = [pandas.Series(['m3', 'M2', 'm2', 'P4', 'd5', 'P5', 'M6', 'm7']),
+                  pandas.Series(['M2', 'm2', 'P4', 'd5', 'P5', 'M6', 'm7', 'm3']),
+                  pandas.Series(['m2', 'P4', 'd5', 'P5', 'M6', 'm7', 'm3', 'M2'])]
+        in_val = TestDissonanceIndexer.make_dataframe(u'interval.IntervalIndexer',
+                                                      ['0,1', '0,2', '1,2'],
+                                                      in_val)
+        expected = {'0,1': pandas.Series([nan, 'M2', 'm2', 'P4', 'd5', nan, nan, 'm7']),
+                    '0,2': pandas.Series(['M2', 'm2', 'P4', 'd5', nan, nan, 'm7', nan]),
+                    '1,2': pandas.Series(['m2', 'P4', 'd5', nan, nan, 'm7', nan, 'M2'])}
+        actual = dissonance.DissonanceIndexer(in_val, {u'special_P4': False})
+        actual = actual.run()['dissonance.DissonanceIndexer']
+        self.assertEqual(len(expected), len(actual.columns))
+        for key in expected.iterkeys():
+            self.assertSequenceEqual(list(expected[key].index), list(actual[key].index))
+            for i in xrange(len(expected[key])):
+                # NB: if you get a TypeError here, that means failure
+                self.assertTrue((expected[key].iloc[i] == actual[key].iloc[i]) or
+                                (isnan(expected[key].iloc[i]) and isnan(actual[key].iloc[i])))
+
+    def test_ind_2(self):
+        # A totally realistic multi-part test---special_P4 is True
+        # 1: rather complicated setup
+        parts_index = ['0,1', '0,2', '0,3', '1,2', '1,3', '2,3']
+        offset_index = [0.0, 0.5, 1.0, 2.0, 3.0, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 8.0, 9.0, 9.5]
+        i_event_lists = [['P4', 'M6', 'P8', 'M3', 'P5', 'm3'],
+                         ['P5', 'M6', 'M2', 'M2', nan, 'P4'],
+                         ['m3', 'm6', 'm3', 'P4', 'P8', 'P5'],
+                         ['m3', 'P5', 'P5', 'M3', 'M3', 'P8'],
+                         ['M3', 'M6', 'M3', 'P4', 'P8', 'P5'],
+                         ['m3', 'P5', 'P5', 'M3', 'M3', 'P8'],
+                         [nan, 'P4', 'm6', 'M2', 'P4', 'm3'],
+                         ['m3', 'm6', 'm3', 'P4', 'P1', 'P5'],
+                         [nan, 'P8', 'P5', 'M6', 'M3', 'P5'],
+                         ['M2', 'A4', 'M6', 'M3', 'P5', 'm3'],
+                         ['m3', 'P5', 'P5', 'M3', 'M3', 'P8'],
+                         ['P4', 'M6', 'P8', 'M3', 'P5', 'm3'],
+                         ['m3', 'm6', 'm3', 'P4', 'P8', 'P5'],
+                         ['m6', 'm3', 'm3', 'P5', 'P5', 'P1'],
+                         ['P5', nan, nan, 'm6', 'm6', nan]]
+        i_event_lists = [pandas.Series(x, index=parts_index) for x in i_event_lists]
+        input_df = pandas.DataFrame(i_event_lists, index=offset_index)
+        i_event_lists = [input_df[x] for x in parts_index]
+        input_df = TestDissonanceIndexer.make_dataframe(u'interval.IntervalIndexer',
+                                                        parts_index,
+                                                        i_event_lists)
+        e_event_lists = [[nan, nan, nan, nan, nan, nan],
+                         [nan, nan, 'M2', 'M2', nan, 'P4'],
+                         [nan, nan, nan, 'P4', nan, nan],
+                         [nan, nan, nan, nan, nan, nan],
+                         [nan, nan, nan, 'P4', nan, nan],
+                         [nan, nan, nan, nan, nan, nan],
+                         [nan, nan, nan, 'M2', 'P4', nan],
+                         [nan, nan, nan, 'P4', nan, nan],
+                         [nan, nan, nan, nan, nan, nan],
+                         ['M2', 'A4', nan, nan, nan, nan],
+                         [nan, nan, nan, nan, nan, nan],
+                         [nan, nan, nan, nan, nan, nan],
+                         [nan, nan, nan, 'P4', nan, nan],
+                         [nan, nan, nan, nan, nan, nan],
+                         [nan, nan, nan, nan, nan, nan]]
+        e_event_lists = [pandas.Series(x, index=parts_index) for x in e_event_lists]
+        expected = pandas.DataFrame(e_event_lists, index=offset_index)
+        # 2: run the test
+        actual = dissonance.DissonanceIndexer(input_df, {u'special_P4': True})
+        actual = actual.run()['dissonance.DissonanceIndexer']
+        # 3: check everything out
+        self.assertEqual(len(expected.columns), len(actual.columns))
+        for key in parts_index:
+            self.assertSequenceEqual(list(expected[key].index), list(actual[key].index))
+            for i in xrange(len(expected[key])):
+                # NB: if you get a TypeError here, that means failure
+                self.assertTrue((expected[key].iloc[i] == actual[key].iloc[i]) or
+                                (isnan(expected[key].iloc[i]) and isnan(actual[key].iloc[i])))
 
 
 #--------------------------------------------------------------------------------------------------#
