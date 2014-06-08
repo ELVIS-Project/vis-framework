3fbf5236671dc205317756024e96e53981c49291
Christopher Antila <christopher@antila.ca>
2014-06-08 15:00:44 -0400

    Fix: Revise run_tests.py to Return non-0 Status on Test Failure
    Previously, the script would return 0 unless there was an error with the file
    itself. This was fine for humans reading test results, but it doesn't work with
    Travis-CI, which needs a non-zero status to signify failure, so failing builds
    where showing up as "passing" on Travis.
    
    This also means the script stops on the first failure.

--- run_tests.py
+++ run_tests.py
@@ -5,7 +5,7 @@
 # Program Description:       Measures sequences of vertical intervals.
 #
 # Filename: run_tests.py
-# Purpose: Run the autmated tests for the models in vis.
+# Purpose: Run automated tests for the VIS Framework.
 #
 # Copyright (C) 2012, 2013, 2014 Jamie Klassen, Christopher Antila
 #
@@ -23,68 +23,84 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #--------------------------------------------------------------------------------------------------
 """
-Run the tests for the models
+Run automated tests for the VIS Framework.
 """
 
-VERBOSITY = 2
+VERBOSITY = 0
 
-# Imports
-import unittest
-from vis.tests import test_indexer, test_note_rest_indexer, test_ngram, test_repeat, \
-    test_aggregator, test_interval_indexer, test_frequency_experimenter, test_offset, \
-    test_lilypond, test_dissonance, test_metre
-from vis.tests import test_indexed_piece, test_aggregated_pieces
+from unittest import TextTestRunner
+
+from vis.tests import test_indexer
+from vis.tests import test_note_rest_indexer
+from vis.tests import test_ngram
+from vis.tests import test_repeat
+from vis.tests import test_aggregator
+from vis.tests import test_interval_indexer
+from vis.tests import test_frequency_experimenter
+from vis.tests import test_offset
+from vis.tests import test_lilypond
+from vis.tests import test_dissonance
+from vis.tests import test_metre
+from vis.tests import test_indexed_piece
+from vis.tests import test_aggregated_pieces
 from vis.tests import bwv2_integration_tests as bwv2
-from vis.tests import test_workflow, test_workflow_integration, test_workflow_experiments
+from vis.tests import test_workflow
+from vis.tests import test_workflow_integration
+from vis.tests import test_workflow_experiments
 
-test_list = ( \
-    # Indexer and Subclasses
-    test_indexer.INDEXER_INIT_SUITE,
-    test_indexer.MAKE_RETURN_SUITE,
-    test_indexer.INDEXER_1_PART_SUITE,
-    test_indexer.INDEXER_MULTI_EVENT_SUITE,
-    test_indexer.UNIQUE_OFFSETS_SUITE,
-    test_note_rest_indexer.NOTE_REST_INDEXER_SUITE,
-    test_interval_indexer.INTERVAL_INDEXER_SHORT_SUITE,
-    test_interval_indexer.INTERVAL_INDEXER_LONG_SUITE,
-    test_interval_indexer.INT_IND_INDEXER_SUITE,
-    test_interval_indexer.HORIZ_INT_IND_LONG_SUITE,
-    test_repeat.REPEAT_INDEXER_SUITE,
-    test_ngram.NGRAM_INDEXER_SUITE,
-    test_offset.OFFSET_INDEXER_SINGLE_SUITE,
-    test_offset.OFFSET_INDEXER_MULTI_SUITE,
-    test_lilypond.ANNOTATION_SUITE,
-    test_lilypond.ANNOTATE_NOTE_SUITE,
-    test_lilypond.PART_NOTES_SUITE,
-    test_lilypond.LILYPOND_SUITE,
-    test_dissonance.DISSONANCE_INDEXER_SUITE,
-    test_dissonance.SUSPENSION_INDEXER_SUITE,
-    test_metre.BEATSTRENGTH_INDEXER_SUITE,
-    # Experimenter and Subclasses
-    test_frequency_experimenter.FREQUENCY_FUNC_SUITE,
-    test_frequency_experimenter.FREQUENCY_RUN_SUITE,
-    test_aggregator.COLUMN_AGGREGATOR_SUITE,
-    # IndexedPiece and AggregatedPieces
-    test_indexed_piece.INDEXED_PIECE_SUITE_A,
-    test_indexed_piece.INDEXED_PIECE_SUITE_B,
-    test_indexed_piece.INDEXED_PIECE_PARTS_TITLES,
-    test_aggregated_pieces.AGGREGATED_PIECES_SUITE,
-    # WorkflowManager
-    test_workflow.WORKFLOW_TESTS,
-    test_workflow.GET_DATA_FRAME,
-    test_workflow.EXPORT,
-    test_workflow.EXTRA_PAIRS,
-    test_workflow.SETTINGS,
-    test_workflow.OUTPUT,
-    test_workflow.AUX_METHODS,
-    test_workflow_experiments.INTERVAL_NGRAMS,
-    test_workflow_experiments.INTERVALS,
-    # Integration Tests
-    bwv2.ALL_VOICE_INTERVAL_NGRAMS,
-    test_workflow_integration.INTERVALS_TESTS,
-    test_workflow_integration.NGRAMS_TESTS,
-    )
 
-# Test runner
-for each_test in test_list:
-    unittest.TextTestRunner(verbosity=VERBOSITY).run(each_test)
+THE_TESTS = (# Indexer and Subclasses
+             test_indexer.INDEXER_HARDCORE_SUITE,
+             test_indexer.INDEXER_1_PART_SUITE,
+             test_indexer.INDEXER_MULTI_EVENT_SUITE,
+             test_indexer.UNIQUE_OFFSETS_SUITE,
+             test_indexer.VERT_ALIGNER_SUITE,
+             test_note_rest_indexer.NOTE_REST_INDEXER_SUITE,
+             test_interval_indexer.INTERVAL_INDEXER_SHORT_SUITE,
+             test_interval_indexer.INTERVAL_INDEXER_LONG_SUITE,
+             test_interval_indexer.INT_IND_INDEXER_SUITE,
+             test_interval_indexer.HORIZ_INT_IND_LONG_SUITE,
+             test_repeat.REPEAT_INDEXER_SUITE,
+             test_ngram.NGRAM_INDEXER_SUITE,
+             test_offset.OFFSET_INDEXER_SINGLE_SUITE,
+             test_offset.OFFSET_INDEXER_MULTI_SUITE,
+             test_lilypond.ANNOTATION_SUITE,
+             test_lilypond.ANNOTATE_NOTE_SUITE,
+             test_lilypond.PART_NOTES_SUITE,
+             test_lilypond.LILYPOND_SUITE,
+             test_dissonance.DISSONANCE_INDEXER_SUITE,
+             test_dissonance.SUSPENSION_INDEXER_SUITE,
+             test_metre.BEATSTRENGTH_INDEXER_SUITE,
+             # Experimenter and Subclasses
+             test_frequency_experimenter.FREQUENCY_FUNC_SUITE,
+             test_frequency_experimenter.FREQUENCY_RUN_SUITE,
+             test_aggregator.COLUMN_AGGREGATOR_SUITE,
+             # IndexedPiece and AggregatedPieces\
+             test_indexed_piece.INDEXED_PIECE_SUITE_A,
+             test_indexed_piece.INDEXED_PIECE_SUITE_B,
+             test_indexed_piece.INDEXED_PIECE_PARTS_TITLES,
+             test_aggregated_pieces.AGGREGATED_PIECES_SUITE,
+             # WorkflowManager
+             test_workflow.WORKFLOW_TESTS,
+             test_workflow.GET_DATA_FRAME,
+             test_workflow.EXPORT,
+             test_workflow.EXTRA_PAIRS,
+             test_workflow.SETTINGS,
+             test_workflow.OUTPUT,
+             test_workflow.MAKE_HISTOGRAM,
+             test_workflow.MAKE_LILYPOND,
+             test_workflow.AUX_METHODS,
+             test_workflow_experiments.INTERVAL_NGRAMS,
+             test_workflow_experiments.INTERVALS,
+             # Integration Tests
+             bwv2.ALL_VOICE_INTERVAL_NGRAMS,
+             test_workflow_integration.INTERVALS_TESTS,
+             test_workflow_integration.NGRAMS_TESTS,
+        )
+
+
+if __name__ == '__main__':
+    for each_test in THE_TESTS:
+        result = TextTestRunner(verbosity=VERBOSITY, descriptions=False).run(each_test)
+        if not result.wasSuccessful():
+            raise RuntimeError('Test failure')
