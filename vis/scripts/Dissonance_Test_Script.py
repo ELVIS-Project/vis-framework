5f983b76094c752f1597dc22e615f01013e43b13
Alexander Morgan <alexanderpmorgan@gmail.com>
2015-04-15 16:15:18 -0400

    Refactoring: import indexer funcs from noterest and duration indexers.

--- vis/scripts/Dissonance_Test_Script.py
+++ vis/scripts/Dissonance_Test_Script.py
@@ -3,7 +3,7 @@ import os
 import pandas
 from vis.workflow import WorkflowManager
 from vis.models.indexed_piece import IndexedPiece
-from vis.analyzers.indexers import interval, dissonance
+from vis.analyzers.indexers import interval, dissonance, metre
 from vis.analyzers.experimenters import frequency
 from vis import workflow
 from numpy import nan, isnan
@@ -23,13 +23,20 @@ def main():
     t0 = time.time()
     # piece_path = "/Users/amor/Documents/Code/VIS/vis/tests/corpus/Kyrie.krn"
     # piece_path = '/Users/amor/Downloads/a3_Josquin_DeTousBiens_StrettoCanonTB.xml'
-    # piece_path = "/Users/amor/Documents/Code/VIS/vis/tests/corpus/bwv77.mxl"
-    piece_path = '/Users/amor/Documents/Code/Reimenschnieder/1-026900B_.xml'
+    piece_path = "/Users/amor/Documents/Code/VIS/vis/tests/corpus/bwv603.xml"
+    # piece_path = '/Users/amor/Documents/Code/Reimenschnieder/1-026900B_.xml'
     # piece_path = '/Users/amor/Documents/Code/VIS/vis/tests/corpus/Jos2308.mei'
     # piece_path = '/Users/amor/Documents/Code/VIS/vis/tests/corpus/Sanctus.krn'
     ind_piece = IndexedPiece(piece_path)
 
-    # pdb.set_trace()
+
+    # bwv603 = converter.parse(os.path.join(VIS_PATH, 'tests', 'corpus/bwv603.xml'))
+    # test_part = [bwv603.parts[0], bwv603.parts[1], bwv603.parts[2], bwv603.parts[3]]
+    actual = ind_piece.get_data([metre.DurationIndexer]) #dur_indexer.run()['metre.DurationIndexer']
+    t1 = time.time()
+    print 'Duration Indexer Runtime: ' + str(t1-t0)
+    pdb.set_trace()
+
     setts = {'quality': True, 'simple or compound': 'simple'}
     horiz_setts = {'quality': False, 'simple or compound': 'compound'}
 
@@ -45,8 +52,8 @@ def main():
     part_numbers = range(len(test_piece.parts))
     from vis.analyzers.indexers.noterest import indexer_func as nr_ind_func
     from vis.analyzers.indexers.metre import duration_ind_func as dur_ind_func
-    from vis.analyzers.indexers.metre import beatstrength_ind_func as bs_ind_func
-    from vis.analyzers.indexers.fermata import indexer_func as fm_ind_func
+    # from vis.analyzers.indexers.metre import beatstrength_ind_func as bs_ind_func
+    # from vis.analyzers.indexers.fermata import indexer_func as fm_ind_func
     for x in part_numbers:
         temp_part = test_piece.parts[x]
         fm = []
@@ -59,31 +66,26 @@ def main():
         measure_index = []
         for event in temp_part.recurse():
             if 'GeneralNote' in event.classes:
-                # found_fm = False
-                # for expression in event.expressions:
-                #     if isinstance(expression, expressions.Fermata):
-                #         fm.append('Fermata')
-                #         found_fm = True
-                #         break
-                # if not found_fm:
-                #     fm.append(nan)
-                fm.append(fm_ind_func((event,)))
+                found_fm = False
+                for expression in event.expressions:
+                    if isinstance(expression, expressions.Fermata):
+                        fm.append('Fermata')
+                        found_fm = True
+                        break
+                if not found_fm:
+                    fm.append(nan)
+                # fm.append(fm_ind_func((event,)))
                 for y in event.contextSites():
                     if y[0] is temp_part:
                         fermata_index.append(y[1])
-                # if hasattr(event, 'tie') and event.tie is not None and event.tie.type in ('stop', 'continue'):
-                #     dur[-1] += event.quarterLength
-                #     continue
-                # if event.name != 'rest':
-                #     nr.append(event.nameWithOctave)
-                # else:
-                #     nr.append('Rest')
+                if hasattr(event, 'tie') and event.tie is not None and event.tie.type in ('stop', 'continue'):
+                    dur[-1] += event.quarterLength
+                    continue
                 nr.append(nr_ind_func((event,)))
                 part_index.append(fermata_index[-1])
-                # dur.append(event.quarterLength)
                 dur.append(dur_ind_func((event,)))
-                # bs.append(event.beatStrength)
-                bs.append(bs_ind_func((event,)))
+                bs.append(event.beatStrength)
+                # bs.append(bs_ind_func((event,)))
             elif 'Measure' in event.classes:
                 ms.append(event.measureNumber)
                 measure_index.append(event.offset)   
